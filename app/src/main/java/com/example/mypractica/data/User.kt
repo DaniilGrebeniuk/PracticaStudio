7752ba690f73ad45f91d42269bb34b50eb868ea9
Daniil <Daniildaniil1313@mail.ru>
2025-12-23 19:40:29 +0800

    Cheng User class add implementation viewBinding

--- app/src/main/java/com/example/mypractica/data/User.kt
+++ app/src/main/java/com/example/mypractica/data/User.kt
@@ -1,3 +1,33 @@
 package com.example.mypractica.data
 
-data class User()
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class User(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+    val age: Int
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString()!!,
+        parcel.readInt()
+    )
+
+    override fun describeContents(): Int = 0
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(name)
+        parcel.writeInt(age)
+    }
+    companion object CREATOR : Parcelable.Creator<User>{
+        override fun createFromParcel(parcel: Parcel)= User(parcel)
+
+        override fun newArray(size: Int)=arrayOfNulls<User>(size)
+    }
+}
