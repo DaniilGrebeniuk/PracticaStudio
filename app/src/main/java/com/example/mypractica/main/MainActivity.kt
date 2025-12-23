37d3d41dd00fe0676eb19287a94219c349559f21
Daniil <Daniildaniil1313@mail.ru>
2025-12-23 19:02:43 +0800

    Empty project

---
+++ app/src/main/java/com/example/mypractica/main/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.mypractica.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
